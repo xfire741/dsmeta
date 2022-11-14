dab8c2109c4877018fd0c772fe01da27c3424b99
xfire741 <eduardovictor28@gmail.com>
2022-11-14 15:36:30 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./assets/components/NotificationButton"
 
 function App() {
   return (
+    <>
     <h1>Olá Mundo</h1>
+    <NotificationButton/>
+    </>
   )
 }
 
